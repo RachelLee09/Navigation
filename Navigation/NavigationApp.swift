2101c7fb4800b1f3816d282ea9453f7bbc23b549
Rachel Lee <rachellee3309@hotmail.com>
2024-07-26 10:42:58 -0700

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Scholar on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
