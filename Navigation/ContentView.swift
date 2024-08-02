1aa76e15a11f39eda84a35e95c19d24d758c2e48
Rachel Lee <rachellee3309@hotmail.com>
2024-08-02 11:25:44 -0700

    nav project

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack{
+                List {
+                    NavigationLink(destination:
+                                    SecondView()){
+                        Text("Click me!")}
+                    Menu(/*@START_MENU_TOKEN@*/"Menu"/*@END_MENU_TOKEN@*/) {
+                        NavigationLink(destination: SecondView()){Text("SecondView")}
+                        /*@START_MENU_TOKEN@*/Text("Menu Item 2")/*@END_MENU_TOKEN@*/
+                        /*@START_MENU_TOKEN@*/Text("Menu Item 3")/*@END_MENU_TOKEN@*/
+                        
+                    }
+                }
+                Text("This is the root view 🌲")
+                    .font(.title3)
+                NavigationLink(destination:
+                    SecondView()){
+                    Text("Second View")}
+            }
+            .toolbar{
+                ToolbarItemGroup(placement:.status){
+                
+                }
+            }
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
         }
-        .padding()
     }
 }
 
---
+++ Navigation/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  Navigation
+//
+//  Created by Scholar on 7/26/24.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("You've arrived at the second view 💋")
+    }
+}
+
+#Preview {
+    SecondView()
+}
